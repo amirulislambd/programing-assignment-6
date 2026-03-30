947923d0cb564b069c8e280a5507c3795e51db7e
amirulislambd <amirulislambd313@gmail.com>
2026-03-30 07:52:35 +0600

    added toggle button structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Banner from "./components/Home/Banner/Banner";
 import Navbar from "./components/Home/Navbar/Navbar";
 import Stats from "./components/Home/Stats/Stats";
+import ToggleBTN from "./components/Home/ToggleBTN/ToggleBTN";
 function App() {
   return (
     <>
       <Navbar/>
       <Banner/>
       <Stats/>
+      <ToggleBTN/>
     </>
   );
 }
